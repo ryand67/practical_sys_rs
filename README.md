b105f8679934d5f3f39ca184aa1ee3c1ec471f7b
Ryan Devenney <rwdevenney@gmail.com>
2022-08-02 08:31:26 -0400

    work

---
+++ README.md
@@ -0,0 +1,1 @@
+following along with the practical systems programming for rust developers book
--- ch2/src/main.rs
+++ ch2/src/main.rs
@@ -1,7 +1,27 @@
+use parsemath::parser::{ParseError, Parser};
 use parsemath::*;
+use std::io;
 
 mod parsemath;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome!");
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => match evaluate(input) {
+                Ok(val) => println!("The computed number is {}\n", val),
+                Err(_) => println!("Error evaluating"),
+            },
+            Err(e) => println!("Error! {}", e),
+        }
+    }
+}
+
+fn evaluate(expr: String) -> Result<f64, ParseError> {
+    let expr = expr.split_whitespace().collect::<String>();
+    let mut math_parser = Parser::new(&expr)?;
+    let ast = math_parser.parse()?;
+    println!("Generated AST is {:?}", ast);
+    Ok(ast::eval(ast)?)
 }
